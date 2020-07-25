28b23fc124cfcf4d17ff13f4db8ecf93b521c636
CodingwithJess <thejessiebess@gmail.com>
2020-07-24 22:00:42 -0600

    finished delete option and event click

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 // ======================
 
 // 1st: pull initial budgetItems/lastID from localStorage to set initial variables
-const budgetItems = JSON.parse(localStorage.getItem("budgetItems")) || [];
+let budgetItems = JSON.parse(localStorage.getItem("budgetItems")) || [];
 let lastID = localStorage.getItem("lastID") || 0;
 
 
@@ -17,29 +17,26 @@ const updateStorage = () => {
   localStorage.setItem("lastID", lastID);
 }
 
-// 5th: function to render budgetItems on table; each item should be rendered in this format:
-// <tr data-id="2"><td>Oct 14, 2019 5:08 PM</td><td>November Rent</td><td>Rent/Mortgage</td><td>1300</td><td>Fill out lease renewal form!</td><td class="delete"><span>x</span></td></tr>
-// also, update total amount spent on page (based on selected category):
 const renderItems = (items) => {
   if (!items) items = budgetItems;
   const tbody = $("#budgetItems tbody");
-  for (const item of items){
-    tbody.append(`<tr data-id="2"><td>${item.date}</td><td>${item.name}</td><td>${item.category}</td><td>${item.amount}</td><td>${item.note}</td><td class="delete"><span>x</span></td></tr>
-    `)
+  tbody.empty();
+  let total = 0;
+  for (const {id, date, name, category, amount, notes} of items){
+    tbody.append(`<tr data-id=${id}><td>${date}</td><td>${name}</td><td>${category}</td><td>$${parseFloat(amount).toFixed(2)}</td><td>${notes}</td><td class="delete"><span>x</span></td></tr>`);
+    total += parseFloat(amount);
   }
-}
-// ======================
-// MAIN PROCESS
-// ======================
 
-// 2nd: wire up click event on 'Enter New Budget Item' button to toggle display of form
+  $("#total").text(`$${total.toFixed(2)}`)
+
+}
+
 $("#toggleFormButton, #hideForm").click(function(){
   $("#addItemForm").toggle("slow", function (){
     $("#toggleFormButton").text($(this).is(":visible")? "Hide Form" : "Add New Item")
   });
 });
 
-// 3rd: wire up click event on 'Add Budget Item' button, gather user input and add item to budgetItems array (each item's object should include: id / date / name / category / amount / notes)... then clear the form fields and trigger localStorage update/budgetItems rerender functions, once created
 $("#addItem").click(function(event){
   event.preventDefault();
   
@@ -59,12 +56,30 @@ $("#addItem").click(function(event){
   budgetItems.push(newItem);
   $("input, select").val("");
   updateStorage();
+  renderItems();
 })
 
 // 6th: wire up change event on the category select menu, show filtered budgetItems based on selection
+$("#categoryFilter").change(function(){
+  const category = $(this).val();
 
+  if (category){
+    const filteredItems = budgetItems.filter((item) => category === item.category);
+    renderItems(filteredItems);
+  } else {
+    renderItems();
+  }
+});
 
 // 7th: wire up click event on the delete button of a given row; on click delete that budgetItem
+
+$("#budgetItems").on("click", ".delete", function(){
+  const id = $(this).parents("tr").data("id");
+  const remainingItems = budgetItems.filter ((item) => item.id !== id);
+  budgetItems = remainingItems;
+  updateStorage();
+  renderItems();
+})
 
 renderItems();
 
